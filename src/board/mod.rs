bc3848802e44dad5e56515e34f283b8cb6975934
Jorge Ruiz <contact@jorgeruizdev.com>
2023-04-17 20:15:17 +0200

    ♻️ Move board mod into a folder

--- src/board/mod.rs
+++ src/board/mod.rs
@@ -1,8 +1,10 @@
 use crate::moves::Direction;
+use crate::piece::Color;
 
 use super::errors::OutOfBoundsError;
 use super::piece::Piece;
 use std::cmp;
+use std::collections::HashMap;
 use std::ops::Add;
 
 const ROWS: u32 = 8;
@@ -34,6 +36,9 @@ impl Add for Coord {
     }
 }
 
+////////////////////////////////////////////////
+// BOARD
+////////////////////////////////////////////////
 #[derive(Debug)]
 pub struct Board {
     board: Vec<Vec<Option<Piece>>>,
@@ -123,5 +128,65 @@ impl Board {
             Direction::East | Direction::West => self.n_cols,
             _ => cmp::max(self.n_rows, self.n_cols),
         }
+    }
+}
+
+////////////////////////////////////////////////
+/// BOARD INFO
+////////////////////////////////////////////////
+
+#[derive(Debug)]
+pub struct BoardInfo {
+    /// Current Turn Color
+    pub turn: Color,
+
+    /// Castling rights, where the king can move
+    pub castling: HashMap<Color, Vec<Coord>>,
+
+    /// En passant target square
+    pub en_passant: Option<Coord>,
+
+    /// Halfmove clock - number of halfmoves since the last capture or pawn advance
+    pub halfmove_clock: i32,
+
+    /// Fullmove number - the number of the full move. It starts at 1, and is incremented after Black's move.
+    pub fullmove_number: i32,
+}
+
+impl BoardInfo {
+    pub fn new_default() -> Self {
+        Self {
+            turn: Color::White,
+            castling: HashMap::new(),
+            en_passant: None,
+            halfmove_clock: 0,
+            fullmove_number: 1,
+        }
+    }
+
+    pub fn next_turn(&mut self) {
+        self.turn = match self.turn {
+            Color::White => Color::Black,
+            Color::Black => Color::White,
+        };
+
+        if self.turn == Color::White {
+            self.fullmove_number += 1;
+        }
+
+        self.halfmove_clock += 1;
+    }
+
+    pub fn reset_halfmove_clock(&mut self) {
+        self.halfmove_clock = 0;
+    }
+}
+
+#[derive(Debug)]
+pub struct FenError;
+
+impl Board {
+    pub fn from_fen(fen: &str) -> Result<Self, FenError> {
+        todo!();
     }
 }
