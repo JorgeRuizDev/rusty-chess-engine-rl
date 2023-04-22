b6d3ff3128ac0997f3695cbb65c2d01048618110
Jorge Ruiz <contact@jorgeruizdev.com>
2023-04-22 18:47:40 +0200

    ✨ Add prom & pawn row check methods

--- src/board/board.rs
+++ src/board/board.rs
@@ -3,7 +3,7 @@ use crate::{moves::Direction, notation::FenError};
 use crate::errors::OutOfBoundsError;
 use crate::notation::fen;
 use crate::notation::fen::parse as parse_fen;
-use crate::piece::Piece;
+use crate::piece::{Color, Piece};
 use std::cmp;
 use std::fmt::format;
 
@@ -18,27 +18,12 @@ const COLS: u32 = 8;
 
 pub struct Board {
     board: Vec<Vec<Option<Piece>>>,
-    info: BoardInfo,
+    pub info: BoardInfo,
     n_rows: u32,
     n_cols: u32,
 }
 
 impl Board {
-    pub fn can_move(&self, from: &Coord, to: &Coord) -> bool {
-        let piece = match self.get_piece(from) {
-            Ok(Some(piece)) => piece,
-            _ => return false,
-        };
-
-        for move_ in piece.moves.iter() {
-            if move_.is_move_valid(*from, *to, self) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
     pub fn new(n_rows: Option<u32>, n_cols: Option<u32>) -> Self {
         // Get the default values
         let n_rows = n_rows.unwrap_or(ROWS);
@@ -104,11 +89,26 @@ impl Board {
         Ok(self.board[row as usize][col as usize].as_ref())
     }
 
+    /// Returns the number of cells in the given direction
     pub fn max_cells_direction(&self, direction: &Direction) -> u32 {
         match direction {
             Direction::North | Direction::South => self.n_rows,
             Direction::East | Direction::West => self.n_cols,
             _ => cmp::max(self.n_rows, self.n_cols),
+        }
+    }
+
+    pub fn is_promotion_row(&self, row: i32, color: Color) -> bool {
+        match color {
+            Color::White => row == 0,
+            Color::Black => row == self.n_rows as i32 - 1,
+        }
+    }
+
+    pub fn is_pawn_row(&self, row: i32, color: Color) -> bool {
+        match color {
+            Color::White => row == self.n_rows as i32 - 2,
+            Color::Black => row == 1,
         }
     }
 
@@ -122,6 +122,21 @@ impl Board {
         board.info = info;
 
         Ok(board)
+    }
+
+    pub fn can_move(&self, from: &Coord, to: &Coord) -> bool {
+        let piece = match self.get_piece(from) {
+            Ok(Some(piece)) => piece,
+            _ => return false,
+        };
+
+        for move_ in piece.moves.iter() {
+            if move_.is_move_valid(*from, *to, self) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
 
@@ -189,5 +204,20 @@ mod test {
     fn test_display() {
         let board = Board::default();
         println!("{}", board);
+    }
+
+
+    #[test]
+    fn test_pawn_row(){
+        let board = Board::default();
+        assert!(board.is_pawn_row(1, Color::Black));
+        assert!(board.is_pawn_row(6, Color::White));
+    }
+
+    #[test]
+    fn test_prom_row(){
+        let board = Board::default();
+        assert!(board.is_promotion_row(0, Color::White));
+        assert!(board.is_promotion_row(7, Color::Black));
     }
 }
