23505a612354dd77090f8c406c48726575c7e898
Jorge Ruiz <contact@jorgeruizdev.com>
2023-04-22 12:56:32 +0200

    ✨ Add diagonal possible moves calculations

--- src/moves/diag.rs
+++ src/moves/diag.rs
@@ -70,17 +70,52 @@ impl Move for Diagonal {
         return false;
     }
 
-    fn move_piece(
-        &self,
-        from: crate::board::Coord,
-        to: crate::board::Coord,
-        board: &mut crate::board::Board,
-    ) {
-        todo!()
-    }
-
     fn allowed_moves(&self, from: Coord, board: &Board) -> Vec<Coord> {
-        todo!()
+        let from_piece = match board.get_piece(&from) {
+            Ok(Some(piece)) => piece,
+            _ => return vec![],
+        };
+
+        let mut legal_coords: Vec<Coord> = vec![];
+
+        for direction in [
+            Direction::NorthEast,
+            Direction::NorthWest,
+            Direction::SouthEast,
+            Direction::SouthWest,
+        ] {
+            let step = direction.get_step();
+
+            let mut current_coord = from;
+
+            // for each coord in the direction
+            for _ in 0..self
+                .max_range
+                .unwrap_or(board.max_cells_direction(&direction))
+            {
+                let next_coord = current_coord.clone() + step.clone();
+
+                // Get the next cell
+                let next_piece = match board.get_piece(&next_coord) {
+                    Ok(piece) => piece,
+                    Err(_) => break, // out of bounds -> false
+                };
+
+                match next_piece {
+                    None => legal_coords.push(next_coord), // Empty cell
+                    Some(piece) => {
+                        // if the same color -> invalid
+                        if piece.color != from_piece.color {
+                            legal_coords.push(next_coord);
+                        }
+                        break; // Break -> There is a piece blocking the way (friendly & enemy)
+                    }
+                }
+                current_coord = next_coord;
+            }
+        }
+
+        return legal_coords;
     }
 }
 
@@ -163,5 +198,53 @@ mod tests {
             println!("Testing {:?} -> {:?}", from, to);
             assert_eq!(limit_movement.is_move_valid(from, to, &board), expect);
         }
+    }
+
+    #[test]
+    fn test_bishop_center_board() {
+        let mut board = Board::default();
+        let movement = Rc::new(Diagonal::new(None));
+
+        let bishop = Piece::new(
+            Color::Black,
+            PieceType::Bishop,
+            vec![movement.clone()],
+            Coord { row: 3, col: 3 },
+        );
+        board.set_piece(bishop.clone());
+
+        let moves = movement.allowed_moves(bishop.coord, &board);
+        println!("{:?}", moves);
+        println!("{:?}", &board);
+        assert_eq!(moves.len(), 8);
+    }
+
+    #[test]
+    fn test_white_bishop() {
+        let board = Board::default();
+
+        let from = Coord { row: 7, col: 2 };
+        let movement = Rc::new(Diagonal::new(None));
+
+        let moves = movement.allowed_moves(from, &board);
+        assert_eq!(moves.len(), 0);
+    }
+
+    #[test]
+    fn test_1_step() {
+        let mut board = Board::default();
+        let movement = Rc::new(Diagonal::new(Some(1)));
+
+        let king = Piece::new(
+            Color::Black,
+            PieceType::King,
+            vec![movement.clone()],
+            Coord { row: 4, col: 4 },
+        );
+
+        board.set_piece(king.clone());
+
+        let moves = movement.allowed_moves(king.coord, &board);
+        assert_eq!(moves.len(), 4);
     }
 }
