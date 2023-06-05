1b243b33f432e3e283f77200bac41c7eb3ca6dff
Jorge Ruiz <contact@jorgeruizdev.com>
2023-06-05 17:23:52 +0200

    [check] fix borrow-checker clusterfuck
    It was not possible to retrieve all the pieces references & pass the Board as mutable

--- src/check.rs
+++ src/check.rs
@@ -12,12 +12,7 @@ use crate::{piece::Color, Board, Coord, Piece};
 /// Enemy pieces: All enemy pieces - same color as the piece in coord
 /// Ally pieces: All ally pieces - opposite color as the piece in coord
 /// Returns: true if the move is check and that move does not cause an ally check
-pub fn is_check(
-    coord: &Coord,
-    board: &mut Board,
-    enemy_pieces: &Vec<&Piece>,
-    ally_pieces: &Vec<&Piece>,
-) -> bool {
+pub fn is_check(coord: &Coord, board: &mut Board, is_checking_mate: bool) -> bool {
     let enemy_color = match board.get_piece(coord) {
         Ok(Some(piece)) => piece.color,
         _ => return false, // no piece under check in that cell
@@ -25,24 +20,24 @@ pub fn is_check(
 
     let ally_king_coord = board.get_king(&enemy_color.opposite()).coord;
 
-    // fn checks that after a move, the ally king is not under check
+    // fn checks that after an ally move, the ally king is not under check
     let ally_in_check = |board: &mut Board| -> bool {
-        for piece in enemy_pieces {
-            if piece.can_move(ally_king_coord.clone(), &board) {
-                return true;
-            }
-        }
-        false
+        let enemy_pieces = board.get_all_pieces(&enemy_color);
+
+        return enemy_pieces
+            .iter()
+            .any(|enemy_piece| enemy_piece.can_move(ally_king_coord.clone(), &board));
     };
 
-    for piece in ally_pieces {
+    for piece in board.clone().get_all_pieces(&enemy_color.opposite()) {
         // if an *ally* piece can move to the cell that might be under check.
+        // BUT ...
         if piece.can_move(coord.clone(), &board) {
             let is_ally_in_check =
                 board.temporal_move(&piece.coord, &ally_king_coord, ally_in_check);
 
             // if after moving an ally, the ally king is not under check, then the move is legal
-            if !is_ally_in_check {
+            if !is_ally_in_check || is_checking_mate {
                 return true;
             }
         }
@@ -64,12 +59,7 @@ pub fn is_check(
 ///
 /// Mate in 1: https://lichess.org/editor/r6k/qppppppp/8/8/8/8/PPPPPPPP/K7_b_-_-_0_1?color=white
 /// Is mate: https://lichess.org/editor/r6k/1ppppppp/8/8/8/8/qPPPPPPP/K7_b_-_-_0_1?color=white
-pub fn is_mate(
-    coord: &Coord,
-    board: &mut Board,
-    enemy_pieces: &Vec<&Piece>,
-    ally_pieces: &Vec<&Piece>,
-) -> bool {
+pub fn is_mate(coord: &Coord, board: &mut Board) -> bool {
     let enemy_king = match board.get_piece(coord) {
         Ok(Some(piece)) => piece.clone(), // Clone because i cant borrow with an inmutable board and then use a mutable one.
         _ => return false,                // no piece under check in that cell
@@ -81,9 +71,8 @@ pub fn is_mate(
     // The enemy king can move to a safe cell:
     for to_coord in enemy_king_moves.iter() {
         // fn checks that after a move, the ally king is not under check
-        let is_tmp_move_safe = |new_board: &mut Board| -> bool {
-            !is_check(&to_coord, new_board, enemy_pieces, ally_pieces)
-        };
+        let is_tmp_move_safe =
+            |new_board: &mut Board| -> bool { !is_check(&to_coord, new_board, true) };
 
         let is_move_safe = board.temporal_move(&enemy_king.coord, to_coord, is_tmp_move_safe);
 
@@ -93,20 +82,20 @@ pub fn is_mate(
     }
 
     // Enemy piece can block or capture the piece that is checking the king
-    for piece in enemy_pieces {
-        // if the piece is the same we are checking, skip it
-        if &piece.coord == coord {
+    for enemy_piece in board.clone().get_all_pieces(&enemy_king.color) {
+        // if the piece is the same we are checking mate, skip it
+        if &enemy_piece.coord == coord {
             continue;
         }
 
         // check if any possible move blocks the mate
-        for legal_move in piece.moves.iter() {
-            for to_coord in legal_move.allowed_moves(piece.coord, board) {
-                let tmp_block_capture = |new_board: &mut Board| -> bool {
-                    is_check(&coord, new_board, enemy_pieces, ally_pieces)
-                };
+        for legal_move in enemy_piece.moves.iter() {
+            for to_coord in legal_move.allowed_moves(enemy_piece.coord, board) {
+                let tmp_block_or_capture =
+                    |new_board: &mut Board| -> bool { is_check(&coord, new_board, true) };
 
-                let is_in_check = board.temporal_move(&piece.coord, &to_coord, tmp_block_capture);
+                let is_in_check =
+                    board.temporal_move(&enemy_piece.coord, &to_coord, tmp_block_or_capture);
 
                 if !is_in_check {
                     return false;
@@ -130,111 +119,65 @@ mod tests {
         // Ally pieces -> black
         // Enemy pieces -> white
         // We just moved the pawn to that position
-        let board = Board::from_fen("k7/8/8/8/7R/8/1p6/K7 w - - 0 1").unwrap();
+        let mut board = Board::from_fen("k7/8/8/8/7R/8/1p6/K7 w - - 0 1").unwrap();
 
-        let ally_pieces = &board.get_all_pieces(&Color::Black);
-        let enemy_pieces = &board.get_all_pieces(&Color::White);
-        let white_king = board.get_king(&Color::White);
+        let white_king = board.get_king(&Color::White).coord;
 
-        assert!(is_check(
-            &white_king.coord,
-            &mut board.clone(),
-            enemy_pieces,
-            ally_pieces
-        ));
+        assert!(is_check(&white_king, &mut board, false));
 
-        assert!(!is_mate(
-            &white_king.coord,
-            &mut board.clone(),
-            enemy_pieces,
-            ally_pieces
-        ));
+        assert!(!is_mate(&white_king, &mut board,));
     }
 
     #[test]
     fn not_a_real_check() {
         // Can't move the bishop as it both kings will be in check
-        let board = Board::from_fen("8/8/8/8/R2b3k/8/8/K7 w - - 0 1").unwrap();
+        let mut board = Board::from_fen("8/8/8/8/R2b3k/8/8/K7 w - - 0 1").unwrap();
 
-        let ally_pieces = &board.get_all_pieces(&Color::Black);
-        let enemy_pieces = &board.get_all_pieces(&Color::White);
-        let white_king = board.get_king(&Color::White);
+        let white_king = board.get_king(&Color::White).coord;
 
-        assert!(!is_check(
-            &white_king.coord,
-            &mut board.clone(),
-            enemy_pieces,
-            ally_pieces
-        ));
+        assert!(!is_check(&white_king, &mut board, false));
     }
 
     #[test]
     fn test_default_board() {
-        let board = Board::default();
-        let ally_pieces = &board.get_all_pieces(&Color::Black);
-        let enemy_pieces = &board.get_all_pieces(&Color::White);
-        let white_king = board.get_king(&Color::White);
+        let mut board = Board::default();
+        let white_king = board.get_king(&Color::White).coord;
 
-        assert!(!is_check(
-            &white_king.coord,
-            &mut board.clone(),
-            enemy_pieces,
-            ally_pieces
-        ));
+        assert!(!is_check(&white_king, &mut board, false));
 
-        assert!(!is_mate(
-            &white_king.coord,
-            &mut board.clone(),
-            enemy_pieces,
-            ally_pieces
-        ));
+        assert!(!is_mate(&white_king, &mut board));
     }
 
     // https://lichess.org/editor/1r6/r6k/8/8/4bR2/8/8/K7_w_-_-_0_1?color=white
     #[test]
     fn test_mate() {
-        let board = Board::from_fen("1r6/r6k/8/8/4bR2/8/8/K7 w - - 0 1").unwrap();
+        let mut board = Board::from_fen("1r6/r6k/8/8/4bR2/8/8/K7 w - - 0 1").unwrap();
 
-        let ally_pieces = &board.get_all_pieces(&Color::Black);
-        let enemy_pieces = &board.get_all_pieces(&Color::White);
-        let white_king = board.get_king(&Color::White);
+        let white_king = board.get_king(&Color::White).coord;
 
-        assert!(is_check(
-            &white_king.coord,
-            &mut board.clone(),
-            enemy_pieces,
-            ally_pieces
-        ));
+        assert!(is_check(&white_king, &mut board, false));
 
-        assert!(is_mate(
-            &white_king.coord,
-            &mut board.clone(),
-            enemy_pieces,
-            ally_pieces
-        ));
+        assert!(is_mate(&white_king, &mut board));
     }
 
     // https://lichess.org/editor/1r6/r6k/8/8/4b3/8/5R2/K7_w_-_-_0_1?color=white
     #[test]
     fn test_block_mate() {
-        let board = Board::from_fen("1r6/r6k/8/8/4b3/8/5R2/K7 w - - 0 1").unwrap();
+        let mut board = Board::from_fen("1r6/r6k/8/8/4b3/8/5R2/K7 w - - 0 1").unwrap();
 
-        let ally_pieces = &board.get_all_pieces(&Color::Black);
-        let enemy_pieces = &board.get_all_pieces(&Color::White);
-        let white_king = board.get_king(&Color::White);
+        let white_king = board.get_king(&Color::White).coord;
 
-        assert!(is_check(
-            &white_king.coord,
-            &mut board.clone(),
-            enemy_pieces,
-            ally_pieces
-        ));
+        assert!(is_check(&white_king, &mut board, false));
 
-        assert!(!is_mate(
-            &white_king.coord,
-            &mut board.clone(),
-            enemy_pieces,
-            ally_pieces
-        ));
+        assert!(!is_mate(&white_king, &mut board,));
+    }
+
+    #[test]
+    fn test_block_mate_and_give_check() {
+        let mut board = Board::from_fen("k7/1r6/r5R1/8/8/8/8/K7 w - - 0 1").unwrap();
+        let white_king = board.get_king(&Color::White).coord;
+        assert!(is_check(&white_king, &mut board, false));
+
+        assert!(!is_mate(&white_king, &mut board,));
     }
 }
